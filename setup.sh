c41b2b1132a0c6743f93a336c5f0377bd957701d
Ryan Baker <ryan.da.baker@gmail.com>
2016-03-31 18:19:43 -0400

    changes the setup file

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 sudo apt-get -y update
 sudo apt-get remove --purge node
-sudo apt-get install -y build-essential zlib1g-dev git-core gnupg2 libpq-dev nodejs nodejs npm postgresql python-pip
-sudo ln -s /usr/bin/nodejs /usr/bin/node
+sudo apt-get install -y build-essential zlib1g-dev git-core gnupg2 libpq-dev nodejs npm postgresql python-pip python-dev libffi-dev curl
+npm set registry http://registry.npmjs.org
 sudo npm install -g bower
-pip install -r requirements/dev.txt
+sudo pip install -r requirements/dev.txt
 bower install
