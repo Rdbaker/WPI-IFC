31748804a496c9288522cd3ff88c0ed39f321db6
Ryan Baker <ryan.da.baker@gmail.com>
2016-11-30 10:54:44 -0500

    Fixes a 500 error on the admin pages.

--- ifc/models.py
+++ ifc/models.py
@@ -4,72 +4,43 @@ from flask_admin.contrib.sqla import ModelView
 from flask_login import current_user
 from werkzeug.exceptions import Unauthorized
 
-from ifc.admin.models import Preuser
-from ifc.user.models import Role, User
-from ifc.party.models import Fraternity, Party, Guest
+from ifc.admin.models import Preuser  # noqa
+from ifc.user.models import Role, User  # noqa
+from ifc.party.models import Fraternity, Party, Guest  # noqa
 
 
-class UserModelView(ModelView):
+class AdminModelView(ModelView):
+    def is_accessible(self):
+        """Blocks users that aren't allowed in."""
+        return hasattr(current_user, 'is_site_admin') and \
+            current_user.is_site_admin
+
+    def inaccessible_callback(self, name, **kwargs):
+        """Throws the user to a 401 page if they shouldn't be here."""
+        raise Unauthorized()
+
+
+class UserModelView(AdminModelView):
     column_exclude_list = ['password']
     column_searchable_list = ['username', 'first_name', 'last_name']
     form_excluded_columns = ['password']
 
-    def is_accessible(self):
-        """Blocks users that aren't allowed in."""
-        return current_user.is_site_admin
 
-    def inaccessible_callback(self, name, **kwargs):
-        """Throws the user to a 401 page if they shouldn't be here."""
-        raise Unauthorized()
-
-
-class RoleModelView(ModelView):
+class RoleModelView(AdminModelView):
     can_delete = False
     can_edit = False
 
-    def is_accessible(self):
-        """Blocks users that aren't allowed in."""
-        return current_user.is_site_admin
 
-    def inaccessible_callback(self, name, **kwargs):
-        """Throws the user to a 401 page if they shouldn't be here."""
-        raise Unauthorized()
-
-
-class FraternityModelView(ModelView):
+class FraternityModelView(AdminModelView):
     can_delete = False
 
-    def is_accessible(self):
-        """Blocks users that aren't allowed in."""
-        return current_user.is_site_admin
 
-    def inaccessible_callback(self, name, **kwargs):
-        """Throws the user to a 401 page if they shouldn't be here."""
-        raise Unauthorized()
-
-
-class PreuserModelView(ModelView):
+class PreuserModelView(AdminModelView):
     column_searchable_list = ['username', 'first_name', 'last_name']
     can_delete = False
     can_edit = False
 
-    def is_accessible(self):
-        """Blocks users that aren't allowed in."""
-        return current_user.is_site_admin
 
-    def inaccessible_callback(self, name, **kwargs):
-        """Throws the user to a 401 page if they shouldn't be here."""
-        raise Unauthorized()
-
-
-class PartyModelView(ModelView):
+class PartyModelView(AdminModelView):
     column_searchable_list = ['name']
     can_edit = False
-
-    def is_accessible(self):
-        """Blocks users that aren't allowed in."""
-        return current_user.is_site_admin
-
-    def inaccessible_callback(self, name, **kwargs):
-        """Throws the user to a 401 page if they shouldn't be here."""
-        raise Unauthorized()
